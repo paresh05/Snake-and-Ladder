e02ad9bd0554f5a94de397ada811d8a918c32c30
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-18 23:46:38 +0530

    uc4 added

--- Snakenladder.java
+++ Snakenladder.java
@@ -9,26 +9,35 @@ public class Snakenladder {
 		
 		System.out.println("Welcome to the Snake and Ladder Game!");
 		int positionplayer1 = 0;
-		System.out.println("Roll the Dice Player A");
-		int dice1= ((int) Math.floor(Math.random()*10)%6)+1;
-		System.out.println("Player A, the value is " + dice1);
-		int nextmove1=(int) Math.floor(Math.random()*10)%4;
-		switch(nextmove1) {
-		case NO_PLAY:
-			positionplayer1+=0;
-			System.out.println("No Play");
-			break;
-		case LADDER:
-			positionplayer1+=dice1;
-			System.out.println("Ladder!");
-			break;
-		case SNAKE:
-			positionplayer1-=dice1;
-			System.out.println("Snake");
-			break;
-		default:
-			positionplayer1+=dice1;
-			System.out.println("Move "+dice1);
+		while(positionplayer1<100)
+		{
+			System.out.println("Roll the Dice Player A");
+			int dice1= ((int) Math.floor(Math.random()*10)%6)+1;
+			System.out.println("Player A, the value is " + dice1);
+			int nextmove1=(int) Math.floor(Math.random()*10)%4;
+			switch(nextmove1) {
+			case NO_PLAY:
+				positionplayer1+=0;
+				System.out.println("No Play");
+				break;
+			case LADDER:
+				positionplayer1+=dice1;
+				System.out.println("Ladder!");
+				break;
+			case SNAKE:
+				positionplayer1-=dice1;
+				System.out.println("Snake");
+				break;
+			default:
+				positionplayer1+=dice1;
+				System.out.println("Move "+dice1);
+			}
+			
+			if(positionplayer1<0)
+				positionplayer1=0;
+			System.out.println("The position of Player A is "+positionplayer1);
 		}
+		
+		System.out.println("The Position of Player A is: "+positionplayer1);
 	}
 }
