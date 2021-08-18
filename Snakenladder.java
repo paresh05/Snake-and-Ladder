92201575a668da74017244bbd2153be890f17b0a
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-18 23:30:21 +0530

    uc2 added

--- Snakenladder.java
+++ Snakenladder.java
@@ -6,6 +6,9 @@ public class Snakenladder {
 		
 		System.out.println("Welcome to the Snake and Ladder Game!");
 		int positionplayer1 = 0;
+		System.out.println("Roll the Dice Player A");
+		int dice1= ((int) Math.floor(Math.random()*10)%6)+1;
+		System.out.println("Player A, the value is " + dice1);
 		
 	}
 }
